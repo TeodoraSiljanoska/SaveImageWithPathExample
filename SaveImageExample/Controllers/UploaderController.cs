b500c35050d68de1d8f54601f05e1c4ce529d9df
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-07-21 11:17:00 +0200

    Add project files.

---
+++ SaveImageExample/Controllers/UploaderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SaveImageExample.Models;
+
+namespace SaveImageExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploaderController : ControllerBase
+    {
+        [HttpPost]
+        [Route("UploadFile")]
+        public Response UploadFile([FromForm] FileModel fileModel )
+        {
+            Response response = new Response();
+            try
+            {
+                string path = Path.Combine(@"D:\MyImages", fileModel.FileName);
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    fileModel.file.CopyTo(stream);
+                }
+               
+                response.StatusCode = 200;
+                response.ErrorMessage = "Image Created Successfully.";
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 100;
+                response.ErrorMessage = "Some error occurred" + ex.Message;
+
+            }
+
+            return response;
+        }
+    }
+}
---
+++ SaveImageExample/Models/FileModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SaveImageExample.Models
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+        public IFormFile file { get; set; }
+    }
+}
